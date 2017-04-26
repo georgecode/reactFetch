60be7679a12251d49273855f17bd9b98854cdb6b
georgecode <georgeczurlanis@gmail.com>
2017-04-25 17:16:06 -0700

    Install fetch, es6-Promise, imports-loader and exports-loader. Configured webpack to work with fetch. Preform a json get request and console.log data.

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,25 @@
 var React = require('react');
 
+
+// console.log(fetch)
+
+// console.log("XXXXXXX hello")
+
+
+var xtest = fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(function(response) {
+    return response.json()
+  }).then(function(json) {
+    console.log('parsed json', json)
+    console.log('.title = ', json.title)
+  }).catch(function(ex) {
+    console.log('parsing failed', ex)
+  })
+
+console.log("helllllllooooo")
+console.log(xtest.result)
+console.log("hellllllooooooo")
+
 module.exports = React.createClass({
   render: function() {
     return (
