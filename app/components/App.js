bc352f28fd72b318fc07cc14094002b5ff1b41ea
georgecode <georgeczurlanis@gmail.com>
2017-05-23 03:05:55 -0700

    forgot to commit and push doing so now for reference

--- app/components/App.js
+++ app/components/App.js
@@ -1,33 +1,100 @@
+// 192.168.1.5	AC:BC:32:BE:2E:E3	Cs-MacBook-Pro
+// 192.168.1.8	7C:D1:C3:81:8F:10	MACBOOKPRO-529D
+
+
+
+
+
 var React = require('react');
 
 
 // console.log(fetch)
 
 // console.log("XXXXXXX hello")
-
+var fuck = "xxxfuck"
 
 var xtest = fetch('https://jsonplaceholder.typicode.com/posts/1')
   .then(function(response) {
-    return response.json()
-  }).then(function(json) {
+    return response.json();})
+  .then(function(json) {
     console.log('parsed json', json)
     console.log('.title = ', json.title)
-  }).catch(function(ex) {
+     var fuck = json.title
+  })
+  .catch(function(ex) {
     console.log('parsing failed', ex)
   })
 
+
 console.log("helllllllooooo")
-console.log(xtest.result)
+console.log(xtest)
 console.log("hellllllooooooo")
 
+
+// module.exports = React.createClass({
+//   render: function() {
+//     return (
+//       // <h1>Hello React</h1>
+//       <div className="helloContainer">
+//           <h1 className="hello">Hello React</h1>
+//           <h2>TWO plus TWO equals {2+2}</h2>
+//           <h1>{fuck}</h1>
+//       </div>
+//     )
+//   }
+// });
+
+
+
+
 module.exports = React.createClass({
-  render: function() {
+  getInitialState: function () {
+    return { title: 'Best App' };
+  },
+
+
+
+
+  // fetch('https://jsonplaceholder.typicode.com/posts/1')
+  // .then(function(response) {
+  //   return response.json();})
+  // .then(function(json) {
+  //   console.log('parsed json', json)
+  //   console.log('.title = ', json.title)
+  //    this.setState({ title: json.title });
+  // })
+  // .catch(function(ex) {
+  //   console.log('parsing failed', ex)
+  // }),
+
+
+// this.setState({ title: 'fuuuuuucccccckkkkk' })
+ // tick() {
+ //    this.setState({ time: 'yo yo yo yo' });
+ //  }
+
+ upDateShit: function(){
+ 	console.log("yo yo yo")
+ 	this.replaceState({ title: 'yo yo yo yo' });
+ 	console.log("TEEEESSSSST"+this.state.title)
+
+ },
+
+ // this.upDateShit(),
+
+  
+  render: function () {
+
+  	
     return (
-      // <h1>Hello React</h1>
-      <div className="helloContainer">
-          <h1 className="hello">Hello React</h1>
-          <h2>TWO plus TWO equals {2+2}</h2>
-      </div>
-    )
+
+      <div>
+      
+      <h1>TEST TEST TEST</h1>
+      <h1>
+        {this.state.title}
+      </h1>
+        </div>
+    );
   }
 });
